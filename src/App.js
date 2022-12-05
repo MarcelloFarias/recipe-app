c4c8921de8d55565b3ab079a6760697390a38729
Marcello <marcellorocha632@gmail.com>
2022-12-05 10:11:49 -0300

    Add React Router and setting up route from Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import Home from './Pages/Home/index.js';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-    
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   );
 }
