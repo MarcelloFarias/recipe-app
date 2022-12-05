cd8a54348c0cb60f6e35e61afa4a899cd6702bbe
Marcello <marcellorocha632@gmail.com>
2022-12-05 10:31:30 -0300

    Creating the getRandomRecipe function

--- src/Components/RandomRecipe/index.js
+++ src/Components/RandomRecipe/index.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './style.css';
 
 const RandomRecipe = () => {
+
+    const [ recipe, setRecipe ] = useState({});
+
+    useEffect(() => {
+        getRandomRecipe();
+    }, []);
+
+    async function getRandomRecipe() {
+        await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then((response) => response.json())
+        .then((data) => {
+            setRecipe(data.meals[0]);
+            console.log(data.meals[0]);
+        })
+        .catch((error) => console.log(error))
+    }
+
     return (
         <main>
-            
+
         </main>
     );
 }
