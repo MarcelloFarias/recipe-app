a837888da8fa27400d1dc1fca5ca3b7e889e242a
Marcello <marcellorocha632@gmail.com>
2022-12-05 10:16:08 -0300

    Creating the Header component and importing it to Home page

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+import Header from '../../Components/Header/index.js';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
+
+export default Home;
