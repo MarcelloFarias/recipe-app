0916ce329d77fbc67ef7fe298eeace4d02cc6f65
Marcello <marcellorocha632@gmail.com>
2022-12-05 10:22:03 -0300

    Creating the RandomRecipe component

---
+++ src/Components/RandomRecipe/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+const RandomRecipe = () => {
+    return (
+        <main>
+            
+        </main>
+    );
+}
+
+export default RandomRecipe;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './style.css';
 import Header from '../../Components/Header/index.js';
+import RandomRecipe from '../../Components/RandomRecipe/index.js';
 
 const Home = () => {
     return (
         <>
             <Header />
+            
         </>
     );
 }
