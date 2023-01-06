78dc867a860fad8316bbb24cd2b34b9bbdf706c1
Marcello Farias <marcellorocha632@gmail.com>
2023-01-06 11:38:52 -0300

    Style of the categories select and changing the UseEffect

--- src/Components/Categories/index.js
+++ src/Components/Categories/index.js
@@ -6,21 +6,16 @@ const Categories = () => {
     const [ categories, setCategories ] = useState([]);
 
     useEffect(() => {
-        function getAllCategories() {
-            fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data.categories);
-                data.categories.forEach(category => {
-                    categories.push(category.strCategory);
-                });
-            })
-            .catch((error) => console.log(error));
-    
-            setCategories(categories);
-        }
-
-        getAllCategories();
+        fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.categories);
+            data.categories.forEach(category => {
+                categories.push(category.strCategory);
+                setCategories(categories);
+            });
+        })
+        .catch((error) => console.log(error));
 
     }, [categories]);
 
